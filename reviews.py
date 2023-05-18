196ce0c27e71de0bae5e66ee9157dd73c6f69f9f
Incisivo <103829482+Incisivo@users.noreply.github.com>
2023-05-18 10:36:52 +0100

    Scraping reviews from Tripadvisor
    Para analizar los datos que nos interesan, muchas veces tendremos que crear un código para poder extraer dichos datos. 
    En este caso hemos decidido analizar todos los comentarios (casi 2.000) de un hotel en concreto. 
    Por supuesto, el código se puede reutilizar para el hotel, restaurante o cafetería que deseemos dentro de la misma web.

---
+++ reviews.py
@@ -0,0 +1,66 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import os
+
+# Inicializar Selenium
+driver = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe")
+
+# URL base
+base_url = 'https://www.tripadvisor.es/Hotel_Review-g662606-d285140-Reviews-or'
+base_url2= '-Iberostar_Selection_Anthelia-Costa_Adeje_Adeje_Tenerife_Canary_Islands.html'
+
+# Número de página inicial
+pagina = 0
+lista_comentarios = []
+
+while True:
+    # Crear la URL completa
+    url_completa = base_url + str(pagina) + base_url2
+
+    # Navegar a la URL
+    driver.get(url_completa)
+
+    # Obtener el contenido de la página
+    contenido = driver.page_source
+
+    # Parsear el contenido con BeautifulSoup
+    soup = BeautifulSoup(contenido, 'html.parser')
+
+    # Extraer la información que necesites de la página utilizando bs4
+    
+    comentarios = soup.find_all('span',class_="QewHA H4 _a")
+    for comentario in comentarios:
+        review=comentario.find('span').getText()
+        lista_comentarios.append(review)
+    
+    
+    # Verificar si hay una siguiente página
+    siguiente_pagina = soup.find('a', text='Siguiente')
+
+    mi_path = r"C:\Users\Usuario\Desktop\Nueva carpeta\opiniones-Anthelia.txt"
+    f = open(mi_path, 'a+', encoding="utf-8")
+
+    if siguiente_pagina:
+        # Incrementar el número de página para la siguiente iteración
+        for i in lista_comentarios:
+            f.write(str(i) + '\n')
+            
+        pagina += 5
+    else:
+        # No hay más páginas, salir del bucle
+        break
+    
+
+driver.quit()
+
+
+
+# mi_path = r"C:\Users\Usuario\Desktop\Nueva carpeta\opiniones-Anthelia.txt"
+# f = open(mi_path, 'a+')
+
+# for i in lista_comentarios:
+#     f.write("'" + str(i) + "'" + "," + '\n')
+
+# f.close()
+
